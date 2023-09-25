db88017baac2aab9bea6bc91456d28f6a4f9b602
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2023-09-25 16:25:55 -0300

    fix: revalidate bug swr

--- client/src/context/providers/swr.provider.tsx
+++ client/src/context/providers/swr.provider.tsx
@@ -4,7 +4,7 @@ import { fetcher } from '@/services/fetcher.service'
 import { localStorageProvider } from './localStorageProvider'
 
 const SWRProvider = ({ children }: React.PropsWithChildren<{}>) => (
-  <SWRConfig value={{ provider: localStorageProvider, fetcher, revalidateOnFocus: true, errorRetryCount: 3 }}>{children}</SWRConfig>
+  <SWRConfig value={{ provider: localStorageProvider, fetcher, revalidateOnFocus: true, errorRetryCount: 1 }}>{children}</SWRConfig>
 )
 
 export default SWRProvider
