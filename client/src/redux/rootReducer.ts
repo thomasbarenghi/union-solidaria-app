5849cffccf0c5f3aecc900ba2253734e12049a43
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2023-09-26 00:55:21 -0300

    chore: deprecate services

--- client/src/redux/rootReducer.ts
+++ client/src/redux/rootReducer.ts
@@ -3,14 +3,12 @@ import authSession from './slices/authSession'
 import { initiativesApi } from '@/redux/services/initiatives.service'
 import { currentUsersApi } from '@/redux/services/users.service'
 import { authSessionApi } from '@/redux/services/authSession.service'
-import { reviewsApi } from './services/reviews.service'
 
 const rootReducer = combineReducers({
   authSession,
   [initiativesApi.reducerPath]: initiativesApi.reducer,
   [currentUsersApi.reducerPath]: currentUsersApi.reducer,
-  [authSessionApi.reducerPath]: authSessionApi.reducer,
-  [reviewsApi.reducerPath]: reviewsApi.reducer
+  [authSessionApi.reducerPath]: authSessionApi.reducer
 })
 
 export default rootReducer
