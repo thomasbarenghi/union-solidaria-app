a9d19880ad36681d21f95e30ebf8a7a0cf83fff4
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-10-03 22:48:03 -0300

    refactor(login): refact login form

--- client/src/app/(auth)/login/components/loginForm.tsx
+++ client/src/app/(auth)/login/components/loginForm.tsx
@@ -2,75 +2,108 @@
 'use client'
 import { Input, Button } from '@/components'
 import { useState, useRef } from 'react'
-import { useForm } from 'react-hook-form'
+import { useForm, type SubmitHandler } from 'react-hook-form'
+import { signIn } from 'next-auth/react'
+import GoogleButton from './google'
+import { type LoginFormValues } from '@/interfaces/forms.interface'
 import { useRouter } from 'next/navigation'
-import { usePostLocalLoginMutation } from '@/redux/services/authSession.service'
 import Routes from '@/utils/constants/routes.const'
+import { FetchError } from '@/interfaces/error.interface'
+import { FetchLoginStatus } from '@/types/fetch-status'
 
 const LoginForm = () => {
-  const router = useRouter()
   const [visibility] = useState(false)
-  const [login] = usePostLocalLoginMutation()
   const formRef = useRef<HTMLFormElement>(null)
+  const router = useRouter()
   const {
     register,
-    formState: { errors },
-    handleSubmit
-  } = useForm({
+    formState: { errors, isLoading },
+    handleSubmit,
+    setError
+  } = useForm<LoginFormValues>({
     mode: 'onChange'
   })
 
-  const onSubmit = async (data: any) => {
-    console.log(data)
+  const onSubmit: SubmitHandler<LoginFormValues> = async (data) => {
+    console.log('data')
     try {
-      await login(data).unwrap()
+      // 'status' and 'ok' properties of signIn method return does not work correctly
+      // docs about signIn() https://next-auth.js.org/getting-started/client#using-the-redirect-false-option
+      // docs about open issue https://github.com/nextauthjs/next-auth/issues/7638
+      const responseNextAuth = await signIn('credentials', {
+        email: data.email,
+        password: data.password,
+        redirect: false
+      })
+
+      if (responseNextAuth?.error !== undefined) {
+        const error: FetchError<FetchLoginStatus> = JSON.parse(responseNextAuth?.error as string)
+        const { status, message } = error
+
+        if (status === 401) {
+          console.error(message)
+          setError('root', { type: 'custom', message: 'Credenciales inválidas' })
+        }
+        if (status === 500) {
+          console.error(message)
+          setError('root', { type: 'custom', message: 'No se pudo lograr la conexión con el servidor' })
+        }
+        if (status === 'unhandled') {
+          console.error(message)
+          setError('root', { type: 'custom', message: 'Error inesperado, intentelo de nuevo' })
+        }
+      }
+
       router.push(Routes.HOME)
     } catch (error) {
-      console.log(error)
-      alert('Error al iniciar sesión')
+      console.error(error)
     }
   }
 
   return (
-    <form className='mx-auto flex max-w-sm flex-col items-center gap-4' onSubmit={handleSubmit(onSubmit)} ref={formRef}>
-      <Input
-        type='email'
-        name='email'
-        label='Email'
-        placeholder='Email'
-        hookForm={{
-          register,
-          validations: {
-            pattern: {
-              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-              message: 'Debe ser un email valido'
-            },
-            required: { value: true, message: 'Este campo es requerido' }
-          }
-        }}
-        errorMessage={errors?.email?.message?.toString()}
-      />
-      <div className='relative flex w-full items-center'>
+    <div className='mx-auto flex max-w-sm flex-col items-center gap-4'>
+      <form className='flex flex-col items-center gap-4' onSubmit={handleSubmit(onSubmit)} ref={formRef}>
         <Input
-          type={visibility ? 'text' : 'password'}
-          name='password'
-          label='Contraseña'
-          placeholder='Contraseña'
+          type='email'
+          name='email'
+          label='Email'
+          placeholder='Email'
           hookForm={{
             register,
             validations: {
               pattern: {
-                value: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/,
-                message: 'La contraseña debe tener al menos 8 caracteres, una mayuscula, una minuscula y un numero'
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                message: 'Debe ser un email valido'
               },
               required: { value: true, message: 'Este campo es requerido' }
             }
           }}
-          errorMessage={errors?.password?.message?.toString()}
+          errorMessage={errors?.email?.message?.toString()}
         />
-      </div>
-      <Button type='submit' title='Iniciar sesion' fullWidth />
-    </form>
+        <div className='relative flex w-full items-center'>
+          <Input
+            type={visibility ? 'text' : 'password'}
+            name='password'
+            label='Contraseña'
+            placeholder='Contraseña'
+            hookForm={{
+              register,
+              validations: {
+                required: { value: true, message: 'Este campo es requerido' }
+              }
+            }}
+            errorMessage={errors?.password?.message?.toString()}
+          />
+        </div>
+        <Button type='submit' isLoading={isLoading} title='Iniciar sesion' fullWidth />
+      </form>
+      <GoogleButton />
+      {errors.root !== undefined ? (
+        <p className='rounded-xl border border-red-600 bg-red-600 bg-opacity-40 px-3 py-1 text-sm text-red-800'>
+          {errors.root?.message}
+        </p>
+      ) : null}
+    </div>
   )
 }
 
