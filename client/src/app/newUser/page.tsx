5dfcd34ea026f9b163bf0c8abedb065189582346
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-10-10 15:09:41 -0300

    feat: create newUser page

---
+++ client/src/app/newUser/page.tsx
@@ -0,0 +1,8 @@
+const Page = async () => (
+  <div>
+    <h1>NEW USER</h1>
+    <p>Pagina que se renderiza si se define en la configuracion de NextAuth la redireccion a la pagina new user.</p>
+  </div>
+)
+
+export default Page
