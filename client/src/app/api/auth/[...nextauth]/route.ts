3d45b8a52d948e5af1f972738effb7d0d629ed62
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-10-10 15:07:31 -0300

    feat(auth): comment google provider code

--- client/src/app/api/auth/[...nextauth]/route.ts
+++ client/src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,14 @@
-import CredentialsProvider from 'next-auth/providers/credentials'
-import GoogleProvider from 'next-auth/providers/google'
-import NextAuth from 'next-auth'
+import { userAdapter } from '@/adapters/user.adapt'
 import { loginUser } from '@/services/auth/login.service'
-import { signupUser } from '@/services/auth/signup.service'
-import { getUserByEmail } from '@/services/user/get-user.service'
-import { RegisterFormValues } from '@/interfaces/forms.interface'
+import NextAuth from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+// import { RegisterFormValues } from '@/interfaces/forms.interface'
+// import { signupUser } from '@/services/auth/signup.service'
+// import { getUserByEmail } from '@/services/user/get-user.service'
+// import clientPromise from '@/utils/mongo/client'
+// import { MongoDBAdapter } from '@auth/mongodb-adapter'
+// import { Adapter } from 'next-auth/adapters'
+// import GoogleProvider from 'next-auth/providers/google'
 
 const handler = NextAuth({
   providers: [
@@ -19,66 +23,94 @@ const handler = NextAuth({
         const password = credentials?.password ?? ''
 
         const { user, error } = await loginUser(email, password)
+
         if (error !== null) throw new Error(JSON.stringify(error))
-        return user
+        const { _id, ...adaptedUser } = userAdapter(user)
+        return { ...adaptedUser, id: _id }
       }
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
     })
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
+    // })
   ],
   pages: {
     signIn: '/login'
-  },
-  callbacks: {
-    // entra a jwt y session una vez que pasa signIng, si devuelve false no se accede a estas funciones
-    jwt: async (arg) => {
-      // console.log('JWT: ', arg)
-      // arg -> token, user, account, profile
-      const { token, user } = arg
-      return { ...token, ...user }
-    },
-    session: async (arg) => {
-      // console.log('SESSION: ', arg)
-      // arg -> session, token
-      const { session, token } = arg
-      session.user = token as any
-      return session
-    },
-    signIn: async (arg) => {
-      // console.log('SIGNIN: ', arg)
-      const { account, profile } = arg
-      try {
-        if (account?.provider === 'google') {
-          const { user } = await getUserByEmail(profile?.email as string)
-          if (user !== undefined) return true
-
-          if (profile !== undefined) {
-            const registerData: RegisterFormValues = {
-              firstName: 'Manu Google',
-              lastName: 'Fernandez Google',
-              birthday: new Date(1997, 3, 14),
-              email: profile.email as string,
-              password: 'Test1234',
-              phone: '+54 11 12345678',
-              role: 'volunteer',
-              username: profile.sub as string
-            }
-
-            // TODO: handle res
-            const { error } = await signupUser(registerData)
-            if (error !== null) return false
-            return true
-          }
-        }
-        return true
-      } catch (error) {
-        console.log(error)
-        return false
-      }
-    }
+    // newUser: '/newUser'
   }
+  // adapter: MongoDBAdapter(clientPromise) as Adapter,
+  // callbacks: {
+  //   jwt: async (arg) => {
+  //     // console.log('JWT: ', arg)
+  //     // arg -> token, user, account, profile
+  //     const { token, user, account } = arg
+
+  //     // arg props vary according to the provider
+  //     if (account?.provider === 'google') {
+  //       const { access_token: accessToken, id_token: idToken } = account
+
+  //       return { ...token, ...user, accessToken, idToken }
+  //     }
+
+  //     return token
+  //   },
+  //   session: async (arg) => {
+  //     // console.log('SESSION: ', arg)
+  //     // arg -> session, token
+  //     const { session, token } = arg
+  //     const { user, error } = await getUserByEmail(token.email as string)
+
+  //     if (error === null && user !== undefined) {
+  //       const { _id, ...adaptedUser } = userAdapter(user)
+  //       session.user = {
+  //         ...session.user,
+  //         ...adaptedUser,
+  //         id: _id
+  //       }
+  //       session.token = {}
+  //       session.token.accessToken = token.accessToken
+  //       session.token.idToken = token.idToken
+  //     }
+
+  //     return session
+  //   },
+  //   signIn: async (arg) => {
+  //     // console.log('SIGNIN: ', arg)
+  //     const { account, profile } = arg
+  //     try {
+  //       if (account?.provider === 'google') {
+  //         const { user } = await getUserByEmail(profile?.email as string)
+
+  //         if (user !== undefined) return true
+
+  //         if (profile !== undefined) {
+  //           const registerData: RegisterFormValues = {
+  //             firstName: profile.given_name,
+  //             lastName: profile.family_name,
+  //             birthday: new Date(1997, 3, 14),
+  //             email: profile.email as string,
+  //             password: 'Test1234',
+  //             phone: '+54 11 123456789',
+  //             role: 'volunteer',
+  //             username: profile.sub as string
+  //           }
+
+  //           // TODO: handle res
+  //           const { error } = await signupUser(registerData)
+  //           if (error !== null) return false
+  //           return true
+  //         }
+  //       }
+  //       return true
+  //     } catch (error) {
+  //       console.log(error)
+  //       return false
+  //     }
+  //   }
+  // },
+  // session: {
+  //   strategy: 'jwt'
+  // }
 })
 
 export { handler as GET, handler as POST }
